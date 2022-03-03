f760bc47d3f1dd75619d15bb817d0519e2596c4e
Karl Lee <poogooflupduck@gmail.com>
2022-03-03 21:46:46 +0000

    reconfig default toolbar

--- components/SwrChart.js
+++ components/SwrChart.js
@@ -51,36 +51,47 @@ const SwrChart = (props) => {
             ? {
                 ...props.options.toolbar,
                 controls: [
-                  props.sql && {
-                    type: "Custom",
-                    clickFunction: () => {
-                      setSqlOpen(true);
-                    },
-                    iconSVG: {
-                      content:
-                        '<path d="M24 21V9h-2v14h8v-2h-6zM18 9h-4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1v2a2 2 0 0 0 2 2h2v-2h-2v-2h1a2 2 0 0 0 2-2V11a2 2 0 0 0-2-2Zm-4 12V11h4v10ZM8 23H2v-2h6v-4H4a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2h6v2H4v4h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2Z"/><path data-name="&lt;Transparent Rectangle&gt;" style="fill:none" d="M0 0h32v32H0z"/>',
-                    },
-                  },
-                  props.endpoint && {
-                    type: "Custom",
-                    clickFunction: () => {
-                      window.open(props.endpoint, "_blank").focus();
-                    },
+                  ...(props.options.toolbar.controls &&
+                  props.options.toolbar.controls.length
+                    ? props.options.controls
+                    : [
+                        { type: "Export as CSV" },
+                        { type: "Export as PNG" },
+                        { type: "Export as JPG" },
+                        { type: "Make fullscreen" },
+                        { type: "Reset zoom" },
+                        { type: "Zoom out" },
+                        { type: "Zoom in" },
+                      ]),
+                  ...(props.sql !== null
+                    ? [
+                        {
+                          type: "Custom",
+                          clickFunction: () => {
+                            setSqlOpen(true);
+                          },
+                          iconSVG: {
+                            content:
+                              '<path d="M24 21V9h-2v14h8v-2h-6zM18 9h-4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1v2a2 2 0 0 0 2 2h2v-2h-2v-2h1a2 2 0 0 0 2-2V11a2 2 0 0 0-2-2Zm-4 12V11h4v10ZM8 23H2v-2h6v-4H4a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2h6v2H4v4h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2Z"/><path data-name="&lt;Transparent Rectangle&gt;" style="fill:none" d="M0 0h32v32H0z"/>',
+                          },
+                        },
+                      ]
+                    : []),
+                  ...(props.endpoint !== null
+                    ? [
+                        {
+                          type: "Custom",
+                          clickFunction: () => {
+                            window.open(props.endpoint, "_blank").focus();
+                          },
 
-                    iconSVG: {
-                      content:
-                        '<path d="M8 9H4a2 2 0 0 0-2 2v12h2v-5h4v5h2V11a2 2 0 0 0-2-2Zm-4 7v-5h4v5ZM22 11h3v10h-3v2h8v-2h-3V11h3V9h-8v2zM14 23h-2V9h6a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-4Zm0-7h4v-5h-4Z"/><path data-name="&lt;Transparent Rectangle&gt;" style="fill:none" d="M0 0h32v32H0z"/>',
-                    },
-                  },
-                  ...(props.options.toolbar.controls || [
-                    { type: "Zoom in" },
-                    { type: "Zoom out" },
-                    { type: "Reset zoom" },
-                    { type: "Make fullscreen" },
-                    { type: "Export as CSV" },
-                    { type: "Export as PNG" },
-                    { type: "Export as JPG" },
-                  ]),
+                          iconSVG: {
+                            content:
+                              '<path d="M8 9H4a2 2 0 0 0-2 2v12h2v-5h4v5h2V11a2 2 0 0 0-2-2Zm-4 7v-5h4v5ZM22 11h3v10h-3v2h8v-2h-3V11h3V9h-8v2zM14 23h-2V9h6a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-4Zm0-7h4v-5h-4Z"/><path data-name="&lt;Transparent Rectangle&gt;" style="fill:none" d="M0 0h32v32H0z"/>',
+                          },
+                        },
+                      ]
+                    : []),
                 ],
               }
             : {},
